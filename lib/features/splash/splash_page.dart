029a713d8f819dc6e13bdc0dd31c2187b4590019
Samuel Chan <cchin.samuel@gmail.com>
2022-04-14 15:43:53 +0800

    feat: finish routing when first launch app

--- lib/features/splash/splash_page.dart
+++ lib/features/splash/splash_page.dart
@@ -1,9 +1,11 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:beyondhack/data/repository/auth/auth_repository.dart';
 import 'package:beyondhack/features/auth/pages/login_page.dart';
+import 'package:beyondhack/features/home/home_page.dart';
 import 'package:beyondhack/routes/app_pages.dart';
 import 'package:beyondhack/ui/assets/assets.gen.dart';
 import 'package:beyondhack/ui/utils/ui_helper.dart';
+import 'package:beyondhack/utils/globals.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -18,6 +20,36 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  bool? isLoggedIn;
+
+  @override
+  void initState() {
+    super.initState();
+    // Try to sign in with stored credentials if logged in
+    _reLoginIfAvailable();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (isLoggedIn == null) {
+        logger.wtf('is logged in still null');
+      }
+      if (isLoggedIn ?? false) {
+        Get.offAllNamed(routeName(HomePage));
+      } else {
+        Get.offAllNamed(routeName(LoginPage));
+      }
+    });
+  }
+
+  Future<void> _reLoginIfAvailable() async {
+    try {
+      await AuthRepository.instance.client.auth.signInWithStoredCredentials();
+      logger.i('Logged in');
+      isLoggedIn = true;
+    } catch (e) {
+      isLoggedIn = false;
+      logger.i('Not logged in');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,15 +62,5 @@ class _SplashPageState extends State<SplashPage> {
             .center(),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    // Try to sign in with stored credentials if logged in
-    runAsync(AuthRepository.instance.client.auth.signInWithStoredCredentials());
-    Future.delayed(const Duration(seconds: 2), () {
-      Get.offAllNamed(routeName(LoginPage));
-    });
   }
 }
